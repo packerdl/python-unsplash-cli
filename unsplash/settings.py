19729eb30bbe2d6a2442e2de6828773ab3994dcd
Devin Packer <6426163+packerdl@users.noreply.github.com>
2019-07-20 14:22:58 -0400

    Store and load settings from json file

---
+++ unsplash/settings.py
@@ -0,0 +1,46 @@
+import json
+import os
+
+import click
+
+CONFIG_DIR = click.get_app_dir("unsplash-cli")
+CONFIG_PATH = os.path.join(CONFIG_DIR, "config.json")
+DEFAULT_DIRECTORY = os.path.expanduser(os.path.join("~", "Pictures", "Unsplash"))
+
+# Default configuration parameters
+config = {
+    "directory": DEFAULT_DIRECTORY,
+    "redirect_uri": "http://localhost:8015",
+    "access_key": "",
+    "secret_key": "",
+}
+
+
+def save():
+    if not os.path.isdir(CONFIG_DIR):
+        os.makedirs(CONFIG_DIR)
+    with open(CONFIG_PATH, "w") as fid:
+        json.dump(config, fid, indent=2, sort_keys=True)
+
+
+def load():
+    global config
+    if not os.path.isfile(CONFIG_PATH) or os.path.getsize(CONFIG_PATH) == 0:
+        save()
+    else:
+        with open(CONFIG_PATH, "r") as fid:
+            config_file = json.load(fid)
+            config.update(config_file)
+
+
+def set(key, value):
+    global config
+    config[key] = value
+    save()
+
+
+def show():
+    print(config)
+
+
+load()
