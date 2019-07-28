e203ab9e89dcce837c68909779ae0803fea0f893
Devin Packer <devin.packer@gmail.com>
2019-07-28 14:26:54 -0400

    Show statistics of image directory

---
+++ unsplash/directory.py
@@ -0,0 +1,15 @@
+import os
+
+from click import echo, style
+
+from .settings import config
+from .utils import size_format
+
+
+def info():
+    directory = config["directory"]
+    images = [x for x in os.listdir(directory) if ".jpg" in x]
+    usage = sum(os.path.getsize(x) for x in [os.path.join(directory, img) for img in images])
+    echo("\n" + style("Directory: ", bold=True) + "%s" % directory)
+    echo(style("Number of Images: ", bold=True) + "%d" % len(images))
+    echo(style("Space Usage: ", bold=True) + size_format(usage))
