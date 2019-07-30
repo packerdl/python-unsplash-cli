c71c541e898f98f129af695a5b9372d2a995a398
Devin Packer <devin.packer@gmail.com>
2019-07-29 20:43:47 -0400

    Separate cli functionality

--- unsplash/directory.py
+++ unsplash/directory.py
@@ -1,6 +1,7 @@
 import os
 
 from click import echo, group, style
+from halo import Halo
 
 from .settings import config
 from .utils import size_format
@@ -13,9 +14,19 @@ def directory():
 
 @directory.command()
 def info():
+    with Halo(text="Checking local folder...", spinner="dots"):
+        info = stats()
+    echo("\n" + style("Directory: ", bold=True) + "%s" % info["path"])
+    echo(style("Number of Images: ", bold=True) + "%d" % len(info["images"]))
+    echo(style("Space Usage: ", bold=True) + size_format(info["size"]))
+
+
+def stats():
     directory = config["directory"]
     images = [x for x in os.listdir(directory) if ".jpg" in x]
     usage = sum(os.path.getsize(x) for x in [os.path.join(directory, img) for img in images])
-    echo("\n" + style("Directory: ", bold=True) + "%s" % directory)
-    echo(style("Number of Images: ", bold=True) + "%d" % len(images))
-    echo(style("Space Usage: ", bold=True) + size_format(usage))
+    return {
+        "images": images,
+        "path": directory,
+        "size": usage,
+    }
