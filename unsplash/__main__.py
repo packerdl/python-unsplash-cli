b8a0318863fa4cbd8d6e6c9ce812e3c679676c8b
Devin Packer <6426163+packerdl@users.noreply.github.com>
2019-08-02 19:21:48 -0400

    Add like/unlike commands

--- unsplash/__main__.py
+++ unsplash/__main__.py
@@ -3,13 +3,16 @@ from .auth import login, logout
 from .collection import collection
 from .directory import directory
 from .entry import entry
+from .like import like, unlike
 from .settings import settings
 
 entry.add_command(alias)
 entry.add_command(collection)
 entry.add_command(directory)
+entry.add_command(like)
 entry.add_command(login)
 entry.add_command(logout)
 entry.add_command(settings)
+entry.add_command(unlike)
 
 entry()
