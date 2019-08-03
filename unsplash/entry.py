230bb475330d0214b113d7fdb9cb2de9d2373079
Devin Packer <devin.packer@gmail.com>
2019-08-03 10:18:31 -0400

    refactor: Preemptively avoid circular import
    Utils package imports config from settings. This will cause issues if
    utils are used in the settings package. Move functions that are
    exclusive to downloading images to the default (entry) command module.

--- unsplash/entry.py
+++ unsplash/entry.py
@@ -1,7 +1,11 @@
+import os
+
 import click
+import requests
 from halo import Halo
 
 from . import alias, api, utils
+from .settings import config
 
 
 @click.group(invoke_without_command=True)
@@ -42,9 +46,44 @@ def entry(ctx, **kwargs):
                 kwargs["collections"] = alias.resolve(kwargs["collections"])
             image = api.random(kwargs)
         spinner.text = "Downloading image..."
-        image_path = utils.download(image["id"], image["urls"]["full"])
+        image_path = download(image["id"], image["urls"]["full"])
         utils.set_wallpaper(image_path)
         spinner.succeed(
             "Photo by %s (@%s)" % (image["user"]["name"], image["user"]["username"])
         )
-        utils.pretty_print_info(image)
+        pretty_print_info(image)
+
+
+def download(image_id, image_url=None):
+    if not os.path.isdir(config["directory"]):
+        os.makedirs(config["directory"], exist_ok=True)
+    if not image_url:
+        image = api.photo(image_id)
+        image_url = image["urls"]["full"]
+    api.download_location(image_id)
+    file_path = os.path.join(config["directory"], "%s.jpg" % image_id)
+    with requests.get(image_url, stream=True) as r:
+        r.raise_for_status()
+        with open(file_path, "wb") as fid:
+            for chunk in r.iter_content(chunk_size=8192):
+                if chunk:
+                    fid.write(chunk)
+    return file_path
+
+
+def pretty_print_info(photo):
+    print("")
+    if photo["description"]:
+        click.secho(photo["description"], dim=True)
+    if photo["alt_description"]:
+        click.secho(photo["alt_description"], dim=True)
+    if not photo["description"] and not photo["alt_description"]:
+        click.secho("No description provided", dim=True)
+    print("")
+    click.secho("Downloads ", bold=True, nl=False)
+    click.echo("%d / " % photo["downloads"], nl=False)
+    click.secho("Views ", bold=True, nl=False)
+    click.echo("%d / " % photo["views"], nl=False)
+    click.secho("Likes ", bold=True, nl=False)
+    click.echo(photo["likes"])
+    click.secho(photo["id"], dim=True)
