87404f47edd569a6332b9e9dbc811c188d3e4f45
Devin Packer <6426163+packerdl@users.noreply.github.com>
2019-08-03 10:18:31 -0400

    refactor: Preemptively avoid circular import
    Utils package imports config from settings. This will cause issues if
    utils are used in the settings package. Move functions that are
    exclusive to downloading images to the default (entry) command module.

--- unsplash/utils.py
+++ unsplash/utils.py
@@ -2,28 +2,7 @@ import ctypes
 import os
 import sys
 
-import requests
-from click import echo, secho
-
-from . import api
-from .settings import config
-
-
-def download(image_id, image_url=None):
-    if not os.path.isdir(config["directory"]):
-        os.makedirs(config["directory"], exist_ok=True)
-    if not image_url:
-        image = api.photo(image_id)
-        image_url = image["urls"]["full"]
-    api.download_location(image_id)
-    file_path = os.path.join(config["directory"], "%s.jpg" % image_id)
-    with requests.get(image_url, stream=True) as r:
-        r.raise_for_status()
-        with open(file_path, "wb") as fid:
-            for chunk in r.iter_content(chunk_size=8192):
-                if chunk:
-                    fid.write(chunk)
-    return file_path
+from click import echo, style
 
 
 def size_format(n_bytes):
@@ -61,19 +40,3 @@ def set_wallpaper(image_path):
         )
 
 
-def pretty_print_info(photo):
-    print("")
-    if photo["description"]:
-        secho(photo["description"], dim=True)
-    if photo["alt_description"]:
-        secho(photo["alt_description"], dim=True)
-    if not photo["description"] and not photo["alt_description"]:
-        secho("No description provided", dim=True)
-    print("")
-    secho("Downloads ", bold=True, nl=False)
-    echo("%d / " % photo["downloads"], nl=False)
-    secho("Views ", bold=True, nl=False)
-    echo("%d / " % photo["views"], nl=False)
-    secho("Likes ", bold=True, nl=False)
-    echo(photo["likes"])
-    secho(photo["id"], dim=True)
